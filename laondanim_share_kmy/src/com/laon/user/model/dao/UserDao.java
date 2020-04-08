2a11748c1762e9acacad6653fe4adb0f00d7c41e
aldus <aldus@DESKTOP-KS9M1TG>
2020-04-08 15:43:50 +0900

    프로젝트 초기화

---
+++ laondanim_share_kmy/src/com/laon/user/model/dao/UserDao.java
@@ -0,0 +1,137 @@
+package com.laon.user.model.dao;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Properties;
+import static com.laon.common.JDBCTemplate.close;
+
+import com.laon.user.model.vo.User;
+
+public class UserDao {
+
+private Properties prop=new Properties();
+	//디비 정보 아직 지정전임..
+	public UserDao() {
+		try {
+			String path=UserDao.class.getResource("/sql/user/user-query.properties").getPath();
+			prop.load(new FileReader(path));
+		}catch(IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	
+	
+	public User login(Connection conn,String id,String pw) {
+		//로그인 했을때
+		PreparedStatement pstmt=null;
+		ResultSet rs=null;
+		User u=null;
+		String sql=prop.getProperty("selectUser");
+	try{pstmt=conn.prepareStatement(sql);
+		pstmt.setString(1, id);
+		pstmt.setString(2, pw);
+		rs=pstmt.executeQuery();
+		if(rs.next()){
+			u=new User();
+			u.setUserId(rs.getString("user_id"));
+			u.setName(rs.getString("name"));
+			u.setEmail(rs.getString("email"));
+			
+		}
+		
+	}catch(SQLException e) {
+		e.printStackTrace();
+	}finally {
+		close(rs);
+		close(pstmt);
+	}return u;
+		
+		
+	}
+	
+	public User findId(Connection conn,String name,String email) {
+		//이름,이메일이 일치하는 회원이 있는지 확인 후. 메일보내줘
+		PreparedStatement pstmt=null;
+		ResultSet rs=null;
+		User u=null;
+		String sql=prop.getProperty("findId");
+	try{pstmt=conn.prepareStatement(sql);
+		pstmt.setString(1, name);
+		pstmt.setString(2, email);
+		rs=pstmt.executeQuery();
+		if(rs.next()){
+			u=new User();
+			u.setUserId(rs.getString("user_id"));
+			u.setName(rs.getString("name"));
+			u.setEmail(rs.getString("email"));
+			
+		}
+		
+	}catch(SQLException e) {
+		e.printStackTrace();
+	}finally {
+		close(rs);
+		close(pstmt);
+	}return u;
+		
+		
+	}
+	public User findPw(Connection conn,String id,String email) {
+		//이름,이메일이 일치하는 회원이 있는지 확인 후. 메일보내줘
+		PreparedStatement pstmt=null;
+		ResultSet rs=null;
+		User u=null;
+		String sql=prop.getProperty("findPw");
+	try{pstmt=conn.prepareStatement(sql);
+		pstmt.setString(1, id);
+		pstmt.setString(2, email);
+		rs=pstmt.executeQuery();
+		if(rs.next()){
+			u=new User();
+			u.setUserId(rs.getString("user_id"));
+			u.setEmail(rs.getString("email"));
+			
+		}
+		
+	}catch(SQLException e) {
+		e.printStackTrace();
+	}finally {
+		close(rs);
+		close(pstmt);
+	}return u;
+		
+	}
+
+
+
+	public int updateTemPw(Connection conn,String id, String authenticationKey) {
+		//임시비밀번호로 디비에 변경하는 로직
+		PreparedStatement pstmt=null;
+		int result=0;
+		String sql=prop.getProperty("updateTemPw");
+		try{
+			pstmt=conn.prepareStatement(sql);
+			pstmt.setString(1, authenticationKey);
+			pstmt.setString(2, id);
+			result=pstmt.executeUpdate();
+			}catch(SQLException e) {
+				e.printStackTrace();
+			}finally {
+				
+				close(pstmt);
+
+		}
+		return result;
+
+	
+	}	
+	
+	
+	
+	
+}
---
+++ laondanim_share_kmy/web/index.jsp
@@ -0,0 +1,11 @@
+<%@page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
+
+<%@ include file="/views/common/header.jsp"%>
+
+<section id='content'>
+		<h2 align='center' style='margin-top:200px'>
+			라온다님 메인페이지 !!
+		</h2>
+</section>
+<%@ include file="/views/common/footer.jsp"%> 

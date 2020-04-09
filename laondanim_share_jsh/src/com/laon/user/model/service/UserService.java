9b7094270392ba7cd764a3047a98bcfea335751b
aaaa <aaaa@jsh>
2020-04-10 00:40:34 +0900

    #UserService > 미연언니 Service와 합침
    진호씨 파일까지 세 파일 비교하여 복붙으로 합침.
    진호씨 파일(etc)파일과 차이가 좀 있음.
    
    by 정세현

--- laondanim_share_jsh/src/com/laon/user/model/service/UserService.java
+++ laondanim_share_jsh/src/com/laon/user/model/service/UserService.java
@@ -2,8 +2,8 @@ package com.laon.user.model.service;
 
 import static com.laon.common.template.JDBCTemplate.close;
 import static com.laon.common.template.JDBCTemplate.commit;
-import static com.laon.common.template.JDBCTemplate.getConnection;
 import static com.laon.common.template.JDBCTemplate.rollback;
+import static com.laon.common.template.JDBCTemplate.getConnection;
 
 import java.sql.Connection;
 
@@ -11,8 +11,44 @@ import com.laon.user.model.dao.UserDao;
 import com.laon.user.model.vo.User;
 
 public class UserService {
+	
 	private UserDao dao = new UserDao();
 
+	public User login(String id,String pw) {
+		Connection conn=getConnection();
+		User u=dao.login(conn,id,pw);
+		close(conn);
+		return u;
+
+	}
+	
+	public User findId(String name,String email) {
+		Connection conn=getConnection();
+		User u=dao.findId(conn,name,email);
+		close(conn);
+		return u;
+	}
+	
+	public User findPw(String id,String email) {
+		Connection conn=getConnection();
+		User u=dao.findPw(conn,id,email);
+		close(conn);
+		return u;
+	}
+
+	public int updateTemPw(String id,String authenticationKey) {
+		// 임시비밀번호로 db에 저장하는 로직
+		Connection conn=getConnection();
+		int result=dao.updateTemPw(conn,id,authenticationKey);
+		if(result>0) {
+			commit(conn);
+		}else {
+			rollback(conn);
+		}
+		close(conn);
+		return result;
+	}
+	
 	public boolean userIdDuplicate(String userId) {
 		Connection conn = getConnection();
 		boolean flag = dao.userIdDuplicate(conn, userId);

6a925b67004a6ca1b50dd41d75deb28988706a81
JINS <JINS@JINS-LAPTOP>
2020-04-17 05:54:36 +0900

    # 동행
    주석 삭제
    
    by 승연

--- laondanim_master/src/com/laon/common/TagFilter.java
+++ laondanim_master/src/com/laon/common/TagFilter.java
@@ -135,80 +135,22 @@ public class TagFilter {
 		
 		DonghangTagCountComparator tcc = new DonghangTagCountComparator();
 		
-		System.out.println("====정렬 전====");
-		
-		for(DonghangTagCount t : countList) {
-			System.out.println(t);
-		}
-		
-		//����
-		Collections.sort(countList, tcc);
-		
-		System.out.println("====정렬 후====");
-		
-		for(DonghangTagCount t : countList) {
-			System.out.println(t);
-		}
+//		System.out.println("====정렬 전====");
+//		
+//		for(DonghangTagCount t : countList) {
+//			System.out.println(t);
+//		}
+//		
+//		//����
+//		Collections.sort(countList, tcc);
+//		
+//		System.out.println("====정렬 후====");
+//		
+//		for(DonghangTagCount t : countList) {
+//			System.out.println(t);
+//		}
 		
 		return countList;
 	}
-	/*
-	 * public ArrayList<TagCount> donghangTagCountList(String userTag){
-	 * 
-	 * //userTag = userTag.replaceAll(" ", ""); System.out.println("로그인한 유저 tag : "
-	 * + userTag);
-	 * 
-	 * ArrayList<Donghang> tagList = new DonghangService().selectTagList(); //���
-	 * ����� �Խù��� ���� �±� ������ �±� ����Ʈ�� ����
-	 * 
-	 * String[] userTagArr = userTag.split(","); //�α����� ������ tag String �� �迭��
-	 * �ɰ���
-	 * 
-	 * ArrayList<TagCount> tripTagCountList = new ArrayList<TagCount>();
-	 * 
-	 * int cnt = 0;
-	 * 
-	 * TagCount tagCount = null;
-	 * 
-	 * //����� �Խù� ��ȣ, �±�(�迭) �� tagListArr ����Ʈ�� ���� for(Trip2 t : tagList) {
-	 * int tripNo = t.getNo();
-	 * 
-	 * String stringTag = t.getTag();
-	 * 
-	 * tagCount = new TagCount();
-	 * 
-	 * tagCount.setNo(t.getNo()); tagCount.setUserTbNo(t.getUserTbNo());
-	 * tagCount.setCategory(t.getCategory());
-	 * tagCount.setWriteDate(t.getWriteDate());
-	 * tagCount.setTag(t.getTag().split(",")); //��� ����� �Խù��� tag String �� �迭��
-	 * �ɰ��� ����Ʈ�� ���� tagCount.setTitle(t.getTitle());
-	 * tagCount.setContent(t.getContent());
-	 * tagCount.setTripLocate(t.getTripLocate());
-	 * tagCount.setPeopleNum(t.getPeopleNum());
-	 * tagCount.setTripType(t.getTripType());
-	 * tagCount.setStartDate(t.getStartDate());
-	 * tagCount.setPublicEnabled(t.getPublicEnabled());
-	 * tagCount.setDeleted(t.getDeleted());
-	 * 
-	 * tripTagCountList.add(tagCount); }
-	 * 
-	 * for(TagCount t : tripTagCountList) { //Tag ��ġ�� ī��Ʈ ���� for(String u :
-	 * userTagArr) { if(Arrays.asList(t.getTag()).contains(u)) { //�迭�� �� ���� Ȯ��
-	 * t.setTagCount(t.getTagCount()+1); } } }
-	 * 
-	 * //���� �������̽� Ŭ���� ��ü ���� TagCountComparator tcc = new
-	 * TagCountComparator();
-	 * 
-	 * System.out.println("====정렬 전====");
-	 * 
-	 * for(TagCount t : tripTagCountList) { System.out.println(t); }
-	 * 
-	 * //���� Collections.sort(tripTagCountList, tcc);
-	 * 
-	 * System.out.println("====정렬 후====");
-	 * 
-	 * for(TagCount t : tripTagCountList) { System.out.println(t); }
-	 * 
-	 * return tripTagCountList; }
-	 */
+	
 }
